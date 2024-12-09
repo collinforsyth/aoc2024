58bde1f994ca178d6c476f51c75a46d653fead28
Collin <collin.forsyth@hinge.co>
2024-12-09 11:11:40 -0500

    add benchmarks

--- 09/main_test.go
+++ 09/main_test.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"collinforsyth/aoc2024/util"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+var result int
 
 func TestPart1Sample(t *testing.T) {
 	input := "12345"
@@ -14,7 +17,35 @@ func TestPart1Sample(t *testing.T) {
 	assert.Equal(t, 1928, partOne(parseInput(input2)))
 }
 
+func BenchmarkPart1(b *testing.B) {
+	input, err := util.ReadInput("./input.txt")
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		b.StopTimer()
+		in := parseInput(input.String())
+		b.StartTimer()
+		result = partOne(in)
+	}
+}
+
 func TestPart2Sample(t *testing.T) {
 	input := "2333133121414131402"
 	assert.Equal(t, 2858, partTwo(parseInput(input)))
+}
+
+func BenchmarkPart2(b *testing.B) {
+	input, err := util.ReadInput("./input.txt")
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		b.StopTimer()
+		in := parseInput(input.String())
+		b.StartTimer()
+		result = partTwo(in)
+	}
 }
